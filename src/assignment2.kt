35e94d5ea8ca18923e6879178f80ca1d14e8b887
intwaza <iintwazabelyse@gmail.com>
2021-02-28 00:15:06 +0300

    second assignment

---
+++ src/assignment2.kt
@@ -0,0 +1,24 @@
+fun main() {
+    name()
+    var remainder=(10%6)
+    println(remainder)
+    var sumation=(3+2+5+7)
+    println(sumation)
+    fact()
+}
+fun name(){
+    var name=("hello belyse")
+    println (name)
+}
+fun modulus(num1:Int, num2:Int):Int{
+    var modulus= num1%num2
+    return modulus
+}
+fun add(num1: Int,num2: Int,num3:Int,num4:Int):Int{
+    var add=num1+num2+num3+num4
+    return add
+}
+fun fact(){
+    var fact=("trust myself")
+    println(fact)
+}
