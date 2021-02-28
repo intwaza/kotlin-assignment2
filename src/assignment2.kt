01589ed76aecd285dfbafb42db5f8afab7a053ff
intwaza <iintwazabelyse@gmail.com>
2021-02-28 17:04:49 +0300

    add last commit

--- src/assignment2.kt
+++ src/assignment2.kt
@@ -1,24 +1,29 @@
 fun main() {
     name()
-    var remainder=(10%6)
+    var remainder = modulus(10, 6)
     println(remainder)
-    var sumation=(3+2+5+7)
+    var sumation = add(3, 2, 5, 7)
     println(sumation)
     fact()
 }
-fun name(){
-    var name=("hello belyse")
-    println (name)
+
+fun name() {
+    var name = "Belyse"
+    var greeting = ("hello " + name)
+    println(greeting)
 }
-fun modulus(num1:Int, num2:Int):Int{
-    var modulus= num1%num2
+
+fun modulus(num1: Int, num2: Int): Int {
+    var modulus = num1 % num2
     return modulus
 }
-fun add(num1: Int,num2: Int,num3:Int,num4:Int):Int{
-    var add=num1+num2+num3+num4
+
+fun add(num1: Int, num2: Int, num3: Int, num4: Int): Int {
+    var add = num1 + num2 + num3 + num4
     return add
 }
-fun fact(){
-    var fact=("trust myself")
+
+fun fact() {
+    var fact = ("trust myself")
     println(fact)
 }
